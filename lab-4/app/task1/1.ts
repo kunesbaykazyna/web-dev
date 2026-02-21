135508ec42ac176eeeb9e5d8bd22295d84d0f58c
kunesbay kazyna <k_kunesbay@kbtu.kz>
2026-02-21 20:36:18 +0500

    Add Lab 4

---
+++ lab-4/app/task1/1.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+@Component({
+  selector: 'app-root',
+  template: `Hello`,
+  styles: [`:host { color: blue; }`]
+})
+export class AppComponent { }
---
+++ lab-4/app/task1/2.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` Hello {{city}},{{1+1}}`,
+})
+export class App {
+  city='San-Fracisco';
+
+}
+
+
