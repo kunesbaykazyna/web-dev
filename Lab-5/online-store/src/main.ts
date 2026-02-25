e037f32d0b534eb1c019ac1059cdcd4f16765329
kunesbay kazyna <k_kunesbay@kbtu.kz>
2026-02-25 08:24:04 +0500

    Delete Lab-5 directory

--- Lab-5/online-store/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component'; // Добавьте этот импорт
-
-bootstrapApplication(AppComponent, appConfig) // Передайте AppComponent первым
-  .catch((err) => console.error(err));
