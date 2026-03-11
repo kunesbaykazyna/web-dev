74e127daa57dd6056ac8d06a0b7b2fc41338d32e
kunesbay kazyna <k_kunesbay@kbtu.kz>
2026-03-11 05:24:50 +0500

    Add files via upload

---
+++ lab-4/online-store/src/app/data/products.ts
@@ -0,0 +1,154 @@
+import { Product } from '../models/product.models';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'Смартфон Apple iPhone 15 Pro Max 256Gb серый',
+    description: 'Флагманский смартфон Apple с титановым корпусом, процессором A17 Pro и тройной камерой. Поддерживает динамический остров и USB-C.',
+    price: 649990,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hc1/h65/83559848198174.png',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h32/h91/84605303488542.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/he8/hdd/84605699358750.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hee/h4b/84605699424286.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h30/h4c/84605699489822.png?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/apple-iphone-15-dual-sim-6-gb-256-gb-chernyi-114963265/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_home_smart_house_desktop&gbraid=0AAAAAC7-v7hlp4g3zR9vRQUXf9Q5CNmfw&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1EvIsDH8JVBqJLnmoqQeBbfqckDZuDVP1ROgUyVfEbbi980CE4o2-RoC7JIQAvD_BwE'
+  },
+  {
+    id: 2,
+    name: 'Ноутбук Apple MacBook Air 13 дюйм M1 256Gb золотистый',
+    description: 'Ноутбук с чипом Apple M1, 8‑ядерным CPU и 7‑ядерным GPU. Бесшумная работа, до 18 часов работы без подзарядки.',
+    price: 379990,
+    rating: 4.9,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h73/h81/64097360035870.jpg',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h06/h08/64213171568670.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h2d/h82/83648592183326.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hdb/hb3/64213181169694.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h3a/h0d/64213216755742.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/apple-macbook-air-13-2020-13-3-8-gb-ssd-256-gb-macos-mgn63ru-a-101182724/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_home_smart_house_desktop&gbraid=0AAAAAC7-v7hlp4g3zR9vRQUXf9Q5CNmfw&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1EvIsDH8JVBqJLnmoqQeBbfqckDZuDVP1ROgUyVfEbbi980CE4o2-RoC7JIQAvD_BwE'
+  },
+  {
+    id: 3,
+    name: 'Наушники Apple AirPods Pro 2nd generation белый',
+    description: 'Беспроводные наушники с активным шумоподавлением, адаптивным режимом прозрачности и пространственным аудио.',
+    price: 119990,
+    rating: 4.7,
+    image: 'https://kaspi.kz/shop/p/naushniki-apple-airpods-pro-2nd-generation-with-wireless-magsafe-charging-case-belyi-113677582/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_home_smart_house_desktop&gbraid=0AAAAAC7-v7hlp4g3zR9vRQUXf9Q5CNmfw&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1EvIsDH8JVBqJLnmoqQeBbfqckDZuDVP1ROgUyVfEbbi980CE4o2-RoC7JIQAvD_BwE',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/ha3/h07/84108189630494.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h03/h0e/84108189696030.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h14/h8a/84108189761566.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hf4/hf2/84108189827102.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/apple-airpods-pro-2nd-generation-belyi-106362783/?c=750000000'
+  },
+  {
+    id: 4,
+    name: 'Смартфон Samsung Galaxy S24 Ultra 256Gb черный',
+    description: 'Флагман Samsung с титановой рамкой, S Pen, 200 МП камерой и процессором Snapdragon 8 Gen 3.',
+    price: 579990,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h7c/h38/84963297329182.png?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/hfe/ha8/84963297394718.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hce/h21/84963123462174.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h16/h23/84963123527710.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h85/hb3/84963123593246.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-s24-ultra-5g-12-gb-256-gb-seryi-116043556/?c=750000000'
+  },
+  {
+    id: 5,
+    name: 'Планшет Samsung Galaxy Tab S9 FE 10.9 дюйм 6/128Gb серебристый',
+    description: 'Планшет с 90 Hz дисплеем, защитой от воды IP68 и стилусом S Pen в комплекте.',
+    price: 199990,
+    rating: 4.6,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h16/hbf/86283948130334.png?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p9f/p5f/34494221.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pbc/p5f/34494222.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/p10/p60/34494225.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pf4/p5f/34494224.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-tab-s10-fe-10-9-djuim-8-gb-128-gb-serebristyi-137505496/?c=750000000'
+  },
+  {
+    id: 6,
+    name: 'Фен Dyson Supersonic HD07 фуксия/никель',
+    description: 'Мощный фен с быстрой сушкой, интеллектуальным контролем температуры и магнитными насадками.',
+    price: 179990,
+    rating: 4.9,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hd3/h79/64362589732894.jpg',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h16/hbf/86283948130334.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h23/h43/86283948228638.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc8/h95/86283948294174.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hef/hb9/86283948326942.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/dyson-supersonic-hd08-fen-seryi-rozovyi-107135219/?c=750000000'
+  },
+  {
+    id: 7,
+    name: 'Робот-пылесос Xiaomi Robot Vacuum X20+ белый',
+    description: 'Умный робот-пылесос с лазерной навигацией, функцией влажной уборки и автоматическим поднятием швабры.',
+    price: 129990,
+    rating: 4.7,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p4a/p07/107939309.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/pb6/p09/107939310.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pd2/p09/107939311.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pee/p09/107939312.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p5f/p0a/107939316.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/xiaomi-robot-vacuum-x20-bhr8124eu-belyi-117857029/?c=750000000'
+  },
+  {
+    id: 8,
+    name: 'Кофемашина DeLonghi Magnifica S ECAM 22.110.SB серебристый',
+    description: 'Автоматическая кофемашина со встроенной кофемолкой, приготовлением капучино и регулировкой крепости.',
+    price: 299990,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p24/p85/60216731.jpg?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p24/p85/60216731.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/p5c/p85/60216733.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p78/p85/60216734.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p94/p85/60216735.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/kofemashina-delonghi-magnifica-s-ecam-22-110-sb-serebristyi-4400202/?c=750000000'
+  },
+  {
+    id: 9,
+    name: 'Телевизор LG OLED65C3 165 см черный',
+    description: 'OLED-телевизор 4K с процессором α9 Gen6, поддержкой Dolby Vision и игровыми функциями.',
+    price: 899990,
+    rating: 4.9,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p47/p7d/99130466.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p47/p7d/99130466.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pa5/p79/99130478.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p16/p7a/99130474.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p1d/p77/99130480.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/xiaomi-a-pro-2026-l65mb-apru-165-sm-chernyi-146846851/?c=750000000'
+  },
+  {
+    id: 10,
+    name: 'Умные часы Apple Watch Series 9 45 мм серебристый',
+    description: 'Умные часы с дисплеем Always‑On Retina, чипом S9 и расширенными функциями здоровья.',
+    price: 199990,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hc4/hd7/83874224406558.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h28/h01/83874224472094.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hbc/h43/83874224537630.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc4/hd7/83874224406558.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h28/h01/83874224472094.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/apple-watch-series-9-gps-m-l-45-mm-starlight-bezhevyi-113398424/?c=750000000'
+  }
+];
